db8ff02504d84a7cc6c65b15693e8493c490589d
Stanton Borthwick <stanton.borthwick@gmail.com>
2016-03-30 00:01:36 +1300

    can serach with strings, some logical operators and can view files

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
+
 /* GET home page. */
 router.get("/",function(req,res){
   client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
@@ -12,15 +13,59 @@ router.get("/",function(req,res){
 		if(error){ 
 		  console.error(error);
 		} else {
-		  res.render('index', { title: 'ECS Video Rental', place: result.result });
+		  res.render('index', { title: 'Colenso Project', place: result.result });
 		}
 	  }
 	);
 });
-/* Search Function. */
-router.get('/search', function(req, res) {
-res.render('search', { title: 'My Movie Store', content: req.query.searchString });
+
+/* Search String Function. */
+router.get('/search', function(req, res) {    
+  var searchString = req.query.searchString
+  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; for $n in .//TEI[. contains text "+searchString+"]" + " return db:path($n)",
+  function(error, result) {     
+    if(error) {
+      console.error(error);
+    } else{
+      var content = result.result;
+      console.log(content);
+      content = content.split("\n");
+      console.log(searchString);
+      res.render('search', { title: 'Search Results', query: content });  
+    }
+  });  
 });
 
+/* Search Markup Function. */
+router.get('/searchXQuery', function(req, res) {    
+  var searchMarkup = req.query.searchMarkup
+  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; for $n in" + searchMarkup + " return db:path($n)",
+  function(error, result) {     
+    if(error) {
+      console.error(error);
+    } else{
+      var content = result.result;
+      console.log(content);
+      content = content.split("\n");
+      console.log(searchMarkup);
+      res.render('searchXQuery', { title: 'Search Results', query: content });  
+    }
+  });  
+});
+
+/*routes to newfile*/
+router.get('/displayFile',function(req,res){
+  
+  client.execute("XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0';" + "(doc('Colenso/"+req.query.file+"'))[1]",
+  function (error,result){
+    if(error) {
+      console.error(error);
+    } else{
+        var content = result.result;
+      res.render('displayFile', { title: 'Colenso Project', file: content });  
+    }
+  });
+    
+});
 
 module.exports = router;
